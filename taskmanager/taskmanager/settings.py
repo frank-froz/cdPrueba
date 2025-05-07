50dba6bff56259a17287d1a6470534dff2b9b024
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-07 03:39:58 -0500

    update

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'https://cdprueba.onrender.com',
 ]
 
 ROOT_URLCONF = 'taskmanager.urls'
